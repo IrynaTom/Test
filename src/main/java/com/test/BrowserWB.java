9100fb0715ef6e0383576cfbb42fdf4729f0693d
IrynaTom <tomanova.iryna@gmail.com>
2019-04-09 11:10:05 +0300

    Delete BrowserWB.java

--- src/main/java/com/test/BrowserWB.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class BrowserWB {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://waitbusters-dining-dev.dev.softarex.comL");
-    }
-}
