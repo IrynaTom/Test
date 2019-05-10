27484589a7560dc9eb26cb54287d328de3303b5c
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-05-10 14:45:37 +0300

    Done  Return True

--- src/main/java/com/test/stepic/ReturnTrue.java
+++ src/main/java/com/test/stepic/ReturnTrue.java
@@ -7,7 +7,7 @@ public class ReturnTrue {
 
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
 
-        return ((((a&b)^(c&d))| (a&c)^(b&d)))| (a&d)^(b&c);
+        return ((((a&b)^(c&d))| (a&c)^(b&d)))^ (a&d)^(b&c);
 
     }
 }
