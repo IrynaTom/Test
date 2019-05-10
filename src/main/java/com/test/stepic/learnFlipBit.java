ebee0c2b7bf6f2739e1874cefd6f851daa861de9
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-05-10 18:07:05 +0300

    Done charExpression

--- src/main/java/com/test/stepic/learnFlipBit.java
+++ src/main/java/com/test/stepic/learnFlipBit.java
@@ -1,8 +1,19 @@
 package com.test.stepic;
 
 public class learnFlipBit {
-
-    public static int flipBit(int value, int bitIndex) {
-        return
+    public static void main(String[] args) {
+        System.out.println( charExpression(32));
+       System.out.println( charExpression(29));
     }
+
+   /* public static int flipBit(int value, int bitIndex) {
+
+    }*/
+
+    public static char charExpression(int a) {
+        char solidus = '\\';
+        solidus += a;
+        return solidus;
+    }
+
 }
