d9b4a39f65e8a65849b0c5ffd5f4f4a104e63f4a
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-05-13 12:12:35 +0300

    Add ComplexNumberCode

---
+++ src/main/java/com/test/stepic/ComplexNumberCode.java
@@ -0,0 +1,22 @@
+package com.test.stepic;
+
+public final class ComplexNumberCode {
+    public static void main(String[] args) {
+
+    }
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+}
