c4f93e1781e6c688869a97e688e360bbdb6338d4
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-05-13 15:50:18 +0300

    Edit NumIntegrOfGivenFunction

--- src/main/java/com/test/stepic/NumIntegrOfGivenFunction.java
+++ src/main/java/com/test/stepic/NumIntegrOfGivenFunction.java
@@ -1,20 +1,26 @@
 package com.test.stepic;
+import com.demo.task.calendar.Month;
+
 import java.util.function.DoubleUnaryOperator;
 
 public class NumIntegrOfGivenFunction {
     public static void main(String[] args) {
-        System.out.println(integrate(x->1, 0, 10));
+        System.out.println(integrate(x->2, 0, 10));
     }
 
 
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
-        double gridStep= Math.pow(10, -7);
-      double res=0;
-        double step = (b-a)/gridStep; //шаг сетки
-        for(double i=a; i<=b; i++) {
-            res += (f.applyAsDouble(a+step*i));
+        double gridStep = Math.pow(10,-6);
+        double step = (b-a) / gridStep; //шаг сетки
+        double res=0;
+
+        for(int i=0; i < gridStep; i++) {
+            res += f.applyAsDouble(a+i*step);
         }
-        res += step;
+
+        System.out.println(res);
+        res *= step;
+        System.out.println(res);
        return res;
     }
 
