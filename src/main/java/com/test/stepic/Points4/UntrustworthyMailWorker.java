669d5e24a343c8497ef00f68fac381b0b02f1809
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-05-29 10:42:43 +0300

    Add Point 4 Done UntrustworthyMailWorker

--- src/main/java/com/test/stepic/Points4/UntrustworthyMailWorker.java
+++ src/main/java/com/test/stepic/Points4/UntrustworthyMailWorker.java
@@ -1,11 +1,38 @@
 package com.test.stepic.Points4;
 
-public class UntrustworthyMailWorker implements MailService {
-    public static final String AUSTIN_POWERS = "Austin Powers";
+public  class UntrustworthyMailWorker implements MailService {
+
+
+    private RealMailService realMailService;
+    private MailService[] mailServices;
+
+    public RealMailService getRealMailService() {
+        return realMailService;
+    }
+
+    public UntrustworthyMailWorker(MailService[] mailServices) {
+        this.mailServices = mailServices;
+        for (MailService ms : this.mailServices) {
+            if (ms instanceof RealMailService)
+                this.realMailService = (RealMailService)ms;
+        }
+        if (this.realMailService == null) {
+            this.realMailService = new RealMailService();
+        }
+    }
 
     @Override
     public Sendable processMail(Sendable mail) {
-
+        Sendable m = mail;
+        for (MailService ms : this.mailServices) {
+            if (ms instanceof RealMailService)
+                continue;
+            m = ms.processMail(m);
+        }
+        if (getRealMailService() != null) {
+            m = this.realMailService.processMail(m);
+        }
+        return m;
     }
 
     }
