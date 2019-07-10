736503393b8e18629f1bd686d857025e64eaab20
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-07-10 17:41:37 +0300

    Parentheses Second

--- src/main/java/com/test/stepFP/Parentheses.java
+++ src/main/java/com/test/stepFP/Parentheses.java
@@ -1,9 +1,21 @@
 package com.test.stepFP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Parentheses {
-    public static void main(String[] args) {
+
+    static int[] array;
+    static int n;
+
+
+    public static void main(String[] args) throws IOException {
         String s = "()()((";
-        System.out.println(correct_pare(s));
+       // System.out.println(correct_pare(s));
+
+        rec(0, 0);
+
     }
 
     static boolean correct_pare(String s) {
@@ -19,5 +31,21 @@ public class Parentheses {
             }
         }
         return bal==0;
+    }
+    static void rec(int idx, int bal){
+        if(idx==2*n){
+            if (bal==0){
+                System.out.println();
+                return;
+            }
+        }
+       else{ array[idx]= '(';
+        rec(idx+1, bal+1); }
+        if(bal==0){
+            return;
+        }
+       else { array[idx]= ')';
+        rec(idx+1, bal-1);
+       }
     }
 }
