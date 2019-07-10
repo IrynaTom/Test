aab8a80cf0e2309f9afed96ea6b43afccb9ea4d0
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-07-10 15:39:00 +0300

    split (

--- src/main/java/com/test/stepFP/Splitting_number.java
+++ src/main/java/com/test/stepFP/Splitting_number.java
@@ -1,26 +1,36 @@
 package com.test.stepFP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Splitting_number {
-    int n = 5;
 
-    public static void main(String[] args) {
-    new Splitting_number().rec(0,0,1);
+    static int[] array;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(in.readLine());
+        array = new int[n];
+    rec(0,0,1);
+
     }
 
-     void  rec(int ind, int sum, int last) {
+    public static void rec(int ind, int sum, int last) {
 
         if (sum == n) {
-            System.out.println(last);
-
+            for (int i = 0; i < ind - 1; i++) {
+                System.out.print(array[i] + " + ");
+            }
+            System.out.println(array[ind - 1]);
+            return;
         }
         else {
             for (int i = last; i <= n - sum; i++) {
-                ind = ind + 1;
-                last = i;
-                sum = sum + i;
-                System.out.println(sum + " last" + last);
+                array[ind] = i;
+                rec(ind + 1, sum + i, i);
             }
         }
 
