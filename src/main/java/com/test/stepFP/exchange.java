30fd10d9e29bd0eb7435eb5be59e5b0b340623d6
i.tomanova@infostroy.com.ua <i.tomanova@infostroy.com.ua>
2019-07-11 14:16:35 +0300

    Exchange Done

--- src/main/java/com/test/stepFP/exchange.java
+++ src/main/java/com/test/stepFP/exchange.java
@@ -4,17 +4,22 @@ public class exchange {
     public static void main(String[] args) {
         int[] array = new int[4];
         array[0] = 10;
-        array[1] = 5;
-        array[2] = 2;
+        array[1] = 8;
+        array[2] = 6;
         array[3] = 1;
-
-        int s = 39;
+        int count=0;
+        int s = 25;
         int n = array.length;
         for (int i = 0; i < n; i++) {
+
             int cnt = 0;
             cnt += s / array[i];
+            count = count+cnt;
             s %= array[i];
+
             System.out.println(array[i] + " " + cnt);
+
         }
+        System.out.println("Number of coins= "+count);
     }
 }
